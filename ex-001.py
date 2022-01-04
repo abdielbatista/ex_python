c8a2b0f5c2e698a6a22fd6561d5fbddb8a077ac4
Abdiel Batista dos Santos <abdielb@yahoo.com>
2022-01-03 21:23:27 -0300

    att-001

---
+++ ex-001.py
@@ -0,0 +1,29 @@
+'''Usando apenas duas variáveis inteiras, 
+faça um programa em que solicite ao usuário que
+digite 5 números. Ao final, o programa escreverá na tela 
+o produto de todos os números digitados.'''
+
+def multi (n1, n2):
+    return n1 * n2
+
+p = 1
+n = int(input("Digite um número "))
+p = multi(p, n)
+
+
+n = int(input("Digite um número "))
+p = multi(p, n)
+
+n = int(input("Digite um número "))
+p = multi(p, n)
+
+n = int(input("Digite um número "))
+p = multi(p, n)
+
+n = int(input("Digite um número "))
+p = multi(p, n)
+
+n = int(input("Digite um número "))
+p = multi(p, n)
+
+print(p)
